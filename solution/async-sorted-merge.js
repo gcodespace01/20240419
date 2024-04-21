38da9c8f3d0af3811236ba5c27dd3ffa596f1408
Michael Gobaco <michael@gobaco.com>
2024-04-21 08:09:23 -0700

    Solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,48 @@
 "use strict";
 
 // Print all entries, across all of the *async* sources, in chronological order.
+async function asyncSortedMerge(logSources, printer) {
+  let first = [];
+
+  for (let i = 0; i < logSources.length; i++) {
+    let pop = await logSources[i].popAsync();
+    first.push( {
+      date: pop.date,
+      msg: pop.msg,
+      sourceIndex: i,
+    })
+  }
+
+  while (first.length > 0) {
+    first.sort((a, b) => {
+      return a.date - b.date
+    })
+
+    let entry = first[0];
+    printer.print(entry);
+    first.shift();
+
+    let pop = await logSources[entry.sourceIndex].popAsync();
+    if (pop === false) {
+      continue;
+    }
+
+    first.push( {
+      date: pop.date,
+      msg: pop.msg,
+      sourceIndex: entry.sourceIndex,
+    })
+  }
+
+  printer.done();
+}
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
-  });
+
+    asyncSortedMerge(logSources, printer);
+
+    resolve(console.log("Sync sort complete."));
+  }); // new Promise
+
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,46 @@
 "use strict";
 
+const date = require("Faker/lib/date");
+const LogSource = require("../lib/log-source");
+const Printer = require("../lib/printer");
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+
+  const first = [];
+  for (let i = 0; i < logSources.length; i++) {
+    let entry = logSources[i].pop();
+    first.push( {
+      date: entry.date,
+      msg: entry.msg,
+      sourceIndex: i,
+    }
+    );
+  }
+
+  while (first.length > 0) {
+    first.sort((a, b) => {
+      return a.date - b.date
+    })
+
+    let entry = first[0];
+    printer.print(entry);
+    first.shift();
+
+    let pop = logSources[entry.sourceIndex].pop();
+    if (pop === false) {
+      continue;
+    }
+
+    first.push( {
+      date: pop.date,
+      msg: pop.msg,
+      sourceIndex: entry.sourceIndex,
+    })
+  }
+
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
